43d7ad67798423bb187e5ac86a05ef8d61208966
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-07-28 10:33:32 +0300

    add: step1 complete

---
+++ Makefile
@@ -0,0 +1,13 @@
+.PHONY: gendiff
+install:
+	poetry install
+gendiff:
+	poetry run gendiff -h
+build:
+	poetry build
+publish:
+	poetry publish --dry-run
+package-install:
+	python3 -m pip install --user dist/*.whl
+make lint:
+	poetry run flake8 gendiff
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''gendiff start script'''
+
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
