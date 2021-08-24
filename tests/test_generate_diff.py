b42549bf9db225697eb8140e3b58a5a729bf8907
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-08-25 02:09:23 +0300

    step_4:change true -> True in test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,7 +4,7 @@ first = get_dict_from_json('tests/fixtures/file1.json')
 second = get_dict_from_json('tests/fixtures/file2.json')
 
 def test_generate_diff():
-    expected = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n }'
+    expected = '{\n - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: True\n}'
 
     assert generate_diff(first, second) == expected
 
