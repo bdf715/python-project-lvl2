1ac008108b9c5e6e81c191f1510936b636143d4e
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-08-31 08:23:38 +0300

    step_5: add tests for yaml

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,10 +1,13 @@
 from gendiff.scripts.gendiff import generate_diff
 
-first_file = 'tests/fixtures/file1.json'
-second_file = 'tests/fixtures/file2.json'
+first_json = 'tests/fixtures/file1.json'
+second_json = 'tests/fixtures/file2.json'
+first_yaml = 'tests/fixtures/file1.yaml'
+second_yaml = 'tests/fixtures/file2.yml'
 
 def test_generate_diff():
     expected = '{\n - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: True\n}'
 
-    assert generate_diff(first_file, second_file) == expected
+    assert generate_diff(first_json, second_json) == expected
+    assert generate_diff(first_yaml, second_yaml) == expected
 
