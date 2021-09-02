190393d8ab9a7e9c38fdbd09ab494fbabb505592
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-09-02 10:03:39 +0300

    step_5: added yaml parsing. step completed

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,5 +1,20 @@
 import json
+import yaml
+
+
+def is_yaml(path):
+    return path.endswith('yaml') or path.endswith('yml')
+
+
+def is_json(path):
+    return path.endswith('json')
 
 
 def get_parsed_json(first_path, second_path):
-    return json.load(open(first_path)), json.load(open(second_path))
+    if is_json(first_path) and is_json(second_path):
+        first = json.load(open(first_path))
+        second = json.load(open(second_path))
+    if is_yaml(first_path) and is_yaml(second_path):
+        first = yaml.safe_load(open(first_path))
+        second = yaml.safe_load(open(second_path))
+    return first, second
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,9 +5,13 @@ second_json = 'tests/fixtures/file2.json'
 first_yaml = 'tests/fixtures/file1.yaml'
 second_yaml = 'tests/fixtures/file2.yml'
 
-def test_generate_diff():
-    expected = '{\n - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: True\n}'
 
+expected = '{\n - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: True\n}'
+
+
+def test_generate_diff_json():
     assert generate_diff(first_json, second_json) == expected
-    assert generate_diff(first_yaml, second_yaml) == expected
 
+
+def test_generate_diff_yaml():
+    assert generate_diff(first_yaml, second_yaml) == expected
