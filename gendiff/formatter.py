9cfbd89da82a6e2a5c7e8f0a276c919c6db56653
Aleksandr Efimovskiy <bdf715@gmail.com>
2021-12-03 03:30:57 +0300

    step_6:

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,4 +1,5 @@
 from gendiff.constants import ADDED, REMOVED, SAVED, CHANGED, RECURSIVE
+from pprint import pprint
 
 
 def flatten(tree):
@@ -14,33 +15,37 @@ def flatten(tree):
     return result
 
 
-def formatter_rec(node, shift=-4):
-    '''
-    if type(node['value']) is dict:
+def format_dict(data, shift):
+    if type(data) is dict:
         res = []
+        res.append('{')
         shift += 4
-        res.append(' ' * shift + name + ' :{')
-        res.append(formatter_rec(node['value'], shift))
-        res.append(' ' * shift + '}')
-        return res
-    '''
+        for key, value in data.items():
+            res.append(' ' * shift + str(key) + ': ' + str(format_dict(value, shift)))
+        res.append(' ' * (shift - 4) + '}')
+        result = '\n'.join(flatten(res))
+    else:
+        result = data
+    return result
+
+
+def formatter_rec(node, shift=-4):
     status = node['status']
     name = node['name']
-    
     if status == RECURSIVE:
         res = []
         shift += 4
-        res.append(' ' * shift + name + ' :{')
+        res.append(' ' * shift + name + ': {')
         res.append(list(map(lambda child: formatter_rec(child, shift), node['children'])))
         res.append(' ' * shift + '}')
-        #print(res[0])
+        pprint(flatten(res))
         return '\n'.join(flatten(res))
     elif status == ADDED:
-        return '{2}  + {0}: {1}'.format(name, node['value'], shift * ' ')
+        return '{2}  + {0}: {1}'.format(name, format_dict(node['value'], shift+4), shift * ' ')
     elif status == REMOVED:
-        return '{2}  - {0}: {1}'.format(name, node['value'], shift * ' ')
+        return '{2}  - {0}: {1}'.format(name, format_dict(node['value'], shift+4), shift * ' ')
     elif status == SAVED:
-        return '{2}    {0}: {1}'.format(name, node['value'], shift * ' ')
+        return '{2}    {0}: {1}'.format(name, format_dict(node['value'], shift+4), shift * ' ')
     elif status == CHANGED:
         old_value = node['old_value']
-        return '{3}  - {0}: {1}\n{3}  + {0}: {2}'.format(name, old_value, node['value'], shift * ' ')
+        return '{3}  - {0}: {1}\n{3}  + {0}: {2}'.format(name, format_dict(old_value, shift+4), format_dict(node['value'], shift+4), shift * ' ')
