d5ab5af2422aafc2900c55e29f60c72ecde3bf2a
Aleksandr Efimovskiy <bdf715@gmail.com>
2022-07-15 13:37:50 +0300

    step_7:delete old

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -10,21 +10,20 @@ def test_generate_diff_simple():
 
 def test_generate_diff_nested_json():
     first_path = 'tests/fixtures/file1_nested.json'
-    second_path= 'tests/fixtures/file2_nested.json'
+    second_path = 'tests/fixtures/file2_nested.json'
     output_nested = open('tests/fixtures/output_nested').read().rstrip()
     assert generate_diff(first_path, second_path) == output_nested
 
 
 def test_generate_diff_nested_yaml():
     first_path = 'tests/fixtures/file1_nested.yaml'
-    second_path= 'tests/fixtures/file2_nested.yaml'
+    second_path = 'tests/fixtures/file2_nested.yaml'
     output_nested = open('tests/fixtures/output_nested').read().rstrip()
     assert generate_diff(first_path, second_path) == output_nested
 
-'''
+
 def test_generate_diff_plain():
     first_path = 'tests/fixtures/file1_nested.json'
-    second_path= 'tests/fixtures/file2_nested.json'
-    output = open('tests/fixtures/output_plain').read().rstrip()
-    assert generate_diff(first_path, second_path) == output
-'''
+    second_path = 'tests/fixtures/file2_nested.json'
+    output_plain = open('tests/fixtures/output_plain').read().rstrip()
+    assert generate_diff(first_path, second_path) == output_plain
