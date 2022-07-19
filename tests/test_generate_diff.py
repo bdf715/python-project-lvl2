e41f08bd0f96428e673a30ea8f1122e2f7ff7e4e
Aleksandr Efimovskiy <bdf715@gmail.com>
2022-07-19 08:47:44 +0300

    step_8: complete

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -34,6 +34,6 @@ def test_generate_diff_plain():
 def test_generate_diff_json():
     first_path = 'tests/fixtures/file1_nested.json'
     second_path = 'tests/fixtures/file2_nested.json'
-    output_json = json.load(open('tests/fixtures/output_json'))
+    output_json = open('tests/fixtures/output_json').read().rstrip()
     assert generate_diff(first_path, second_path,
                          format_name='json') == output_json
